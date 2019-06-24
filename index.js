20715ce2d42bf818c5c3910451b549bd5df4b795
Threqt <45086473+Threqt@users.noreply.github.com>
2019-06-24 13:22:44 -0500

    Test command

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const config = require('./botconfig.json')
 const Discord = require('discord.js')
-const ms = require('ms')
+const ms = require('parse-ms')
 const moment = require('moment')
 const rbx = require('noblox.js')
 const db = require('quick.db')
 const bot = new Discord.Client({
   disableEverybody: true
 });
+const embedColor = config.embedColor
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} has successfully been started.`)
@@ -15,4 +16,19 @@ bot.on("ready", async () => {
   })
 })
 
+bot.on("message", async message => {
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+
+  if (message.content.indexOf(config.prefix) !== 0) return;
+
+  if(cmd === `ping`){
+    let msg = await message.channel.send("Testing...")
+    let pingEmbed = new Discord.RichEmbed()
+      .setColor('#FC9D03')
+      .setDescription(`⏱: ${Math.round(msg.createdAt - message.createdAt)}\n⏳: ${Math.round(bot.ping)}`)
+    message.channel.send(pingEmbed)
+  }
+})
 bot.login(process.env.token)
